4fca7325824b276755ed120230bc2218a7671f87
David Chua <david.cys@live.com>
2015-03-14 17:02:08 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dapi::Application.config.secret_key_base = '77189bc18430f770f736b069c2dfc87c19e7ad0ef3263df8a349fea7291a7501a4d2ba48b4ecaf7fa6fafc928f2a9ae4b330c2ac81668702763ce048df2deea5'
